8b0c8c1c5d5408062b1cfe64479a408ac7bfcc6e
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-02-17 15:30:28 -0800

    Add register observer and related data

--- trivia_maze_model.py
+++ trivia_maze_model.py
@@ -1,42 +1,40 @@
 from abc import ABC, abstractmethod
 
-
 class TriviaMazeModel(ABC):
-
+    
+    def __init__(self):
+        self.maze_observers = []
+        
     """
-    I'm not sure if by nature of the class being abstract the concrete
-    class would have to implement the following methods or by having the
-    decorator here is no other way around it. -Sheehan
-    """
-
     @abstractmethod
     def save_game(self):
         pass
-
+    
     @abstractmethod
     def load_game(self):
         pass
-
+    """
+    
     @abstractmethod
     def get_rooms(self):
         pass
-
+    
     @abstractmethod
-    def use_item(self, option, current_room):
+    def use_item(self, item):
         pass
-
-    @abstractmethod
-    def create_adventurer(self):
-        pass
-
+    
     @abstractmethod
     def get_adventurer_hp(self):
         pass
-
+    
     @abstractmethod
     def get_adventurer_coords(self):
         pass
-
+    
     @abstractmethod
-    def move_adventurer(self, command_key):
+    def move_adventurer(self, direction):
+        pass
+    
+    @abstractmethod
+    def register_observer(observer):
         pass
