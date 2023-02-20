3767454d97522a6eb94794aae579c6c64fe9caac
dskarls <dskpersistent@gmail.com>
2023-02-19 17:08:07 -0800

    Add temporary module with mock model

---
+++ mock.py
@@ -0,0 +1,21 @@
+class TextTriviaMazeModel:
+    def save_game(self):
+        pass
+
+    def load_game(self):
+        pass
+
+    def get_rooms(self):
+        pass
+
+    def get_adventurer_hp(self):
+        pass
+
+    def move_adventurer(self, direction):
+        pass
+
+    def use_item(self, item_name):
+        pass
+
+    def register_observer(self):
+        pass
