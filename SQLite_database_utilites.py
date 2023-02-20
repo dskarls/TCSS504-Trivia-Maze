bd486a0157ec470a3ecc24f0aaa23dc4fa552724
Tom Swanson <tjswanson314@gmail.com>
2023-02-20 11:56:03 -0800

    Removed question_id from table creation code.

--- SQLite_database_utilites.py
+++ SQLite_database_utilites.py
@@ -2,7 +2,7 @@ import csv
 import sqlite3
 
 """
-Take a csv file of question and answer data and import it to a SQLite database
+Take a csv file of question and answer data and import it to a SQLite database.
 """
 
 # Open a connection to the database file
@@ -10,7 +10,7 @@ connection = sqlite3.connect('Question_and_Answer_DB.sqlite')
 
 # Create a new table to hold the data
 connection.execute('''CREATE TABLE IF NOT EXISTS questions
-                (question_id INTEGER , category TEXT, qa_type TEXT, difficulty TEXT, question TEXT, 
+                (category TEXT, qa_type TEXT, difficulty TEXT, question TEXT, 
                  option_1 TEXT, option_2 TEXT, option_3 TEXT, option_4 TEXT,
                  answer TEXT, hint TEXT)''')
 
@@ -21,7 +21,7 @@ with open('Lone_Rangers_QA_DB.csv', 'r') as csvfile:
     next(reader)
     # Insert each row into the database
     for row in reader:
-        query = 'INSERT INTO questions VALUES (?,?,?,?,?,?,?,?,?,?,?)'
+        query = 'INSERT INTO questions VALUES (?,?,?,?,?,?,?,?,?,?)'
         connection.execute(query, row)
 
 # Commit the changes to the database and close the connection
