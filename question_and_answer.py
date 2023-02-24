baf5d23df548618533631949e8bd244a6e587188
dskarls <dskpersistent@gmail.com>
2023-02-23 21:13:53 -0800

    BUG: Fix inheritance for concrete Q&A subclasses

--- question_and_answer.py
+++ question_and_answer.py
@@ -36,7 +36,7 @@ class QuestionAndAnswer(ABC):
         return user_answer.lower() == self.correct_answer.lower()
 
 
-class TrueOrFalseQA(QuestionAndAnswer, ABC):
+class TrueOrFalseQA(QuestionAndAnswer):
     """Represents a true or false question and answer."""
 
     def __init__(self, question, correct_answer, category, option_1, option_2):
@@ -51,10 +51,12 @@ class TrueOrFalseQA(QuestionAndAnswer, ABC):
         self.option_1 = option_1
         self.option_2 = option_2
 
-        self.is_true = correct_answer.lower() == 'true'
+        self.is_true = correct_answer.lower() == "true"
 
     def __hash__(self):
-        return hash((self.question, self.correct_answer, self.category, self.is_true))
+        return hash(
+            (self.question, self.correct_answer, self.category, self.is_true)
+        )
 
     def answer_is_correct(self, user_answer):
         """Check if the user's answer is correct.
@@ -70,10 +72,19 @@ class TrueOrFalseQA(QuestionAndAnswer, ABC):
         return "Sorry, no hints available for true or false questions."
 
 
-class MultipleChoiceQA(QuestionAndAnswer, ABC):
+class MultipleChoiceQA(QuestionAndAnswer):
     """Represents a multiple choice question and answer."""
 
-    def __init__(self, question, correct_answer, category, option_1, option_2, option_3, option_4):
+    def __init__(
+        self,
+        question,
+        correct_answer,
+        category,
+        option_1,
+        option_2,
+        option_3,
+        option_4,
+    ):
         """
         :param question (str): The question.
         :param correct_answer (str): The correct answer to the question.
@@ -90,8 +101,17 @@ class MultipleChoiceQA(QuestionAndAnswer, ABC):
         self.option_4 = option_4
 
     def __hash__(self):
-        return hash((self.question, self.correct_answer, self.category, self.option_1, self.option_2, self.option_3,
-                     self.option_4))
+        return hash(
+            (
+                self.question,
+                self.correct_answer,
+                self.category,
+                self.option_1,
+                self.option_2,
+                self.option_3,
+                self.option_4,
+            )
+        )
 
     def answer_is_correct(self, user_answer):
         """Check if the user's answer is correct.
@@ -106,14 +126,22 @@ class MultipleChoiceQA(QuestionAndAnswer, ABC):
         :return: (str) A hint for the question.
         """
         hint = "The correct answer is NOT one of the following choices: \n"
-        incorrect_options = [o for o in [self.option_1, self.option_2, self.option_3, self.option_4] if
-                             o != self.correct_answer]
+        incorrect_options = [
+            o
+            for o in [
+                self.option_1,
+                self.option_2,
+                self.option_3,
+                self.option_4,
+            ]
+            if o != self.correct_answer
+        ]
         hint += "- " + incorrect_options[0] + "\n"
         hint += "- " + incorrect_options[1] + "\n"
         return hint
 
 
-class ShortAnswerQA(QuestionAndAnswer, ABC):
+class ShortAnswerQA(QuestionAndAnswer):
     """Represents a short answer question and answer."""
 
     def __init__(self, question, correct_answer, category):
@@ -145,7 +173,9 @@ class ShortAnswerQA(QuestionAndAnswer, ABC):
         for i in range(num_words):
             word = words[i]
             length = len(word)
-            num_letters_to_show = length // 2 if length % 2 == 0 else length // 2 + 1
+            num_letters_to_show = (
+                length // 2 if length % 2 == 0 else length // 2 + 1
+            )
             indices_to_show = random.sample(range(length), num_letters_to_show)
             indices_to_show.sort()
             prev_index = -1
@@ -162,25 +192,50 @@ class ShortAnswerQA(QuestionAndAnswer, ABC):
 # sample testing code
 if __name__ == "__main__":
     # create an instance of each subclass and test the methods
-    t = TrueOrFalseQA("Do computers run on software?", "True", "General CS", "True", "False")
+    t = TrueOrFalseQA(
+        "Do computers run on software?", "True", "General CS", "True", "False"
+    )
     print(t.get_hint())
     print(t.answer_is_correct("True"))
     print("***** break *****")
-    m = MultipleChoiceQA("Which is NOT part of MVC?", "Database", "Design Patterns", "Model", "View", "Controller",
-                         "Database")
+    m = MultipleChoiceQA(
+        "Which is NOT part of MVC?",
+        "Database",
+        "Design Patterns",
+        "Model",
+        "View",
+        "Controller",
+        "Database",
+    )
     print(m.get_hint())
     print(m.answer_is_correct("Database"))
     print("***** break *****")
-    s = ShortAnswerQA("What does OOP stand for?", "Object-Oriented Programming", "Object-Oriented Programming")
+    s = ShortAnswerQA(
+        "What does OOP stand for?",
+        "Object-Oriented Programming",
+        "Object-Oriented Programming",
+    )
     print(s.get_hint())
     print(s.answer_is_correct("Programming"))
     print("***** break *****")
 
     # Test hashing for TrueOrFalseQA
-    q1 = TrueOrFalseQA("Are computers electronic?", "True", "General CS", "True", "False")
-    q2 = TrueOrFalseQA("Are computers made of cheese?", "False", "General CS", "True", "False")
-    q3 = TrueOrFalseQA("Where home computers invented in 1492?", "False", "General CS", "True", "False")
-    q4 = TrueOrFalseQA("Are computers electronic?", "True", "General CS", "True", "False")
+    q1 = TrueOrFalseQA(
+        "Are computers electronic?", "True", "General CS", "True", "False"
+    )
+    q2 = TrueOrFalseQA(
+        "Are computers made of cheese?", "False", "General CS", "True", "False"
+    )
+    q3 = TrueOrFalseQA(
+        "Where home computers invented in 1492?",
+        "False",
+        "General CS",
+        "True",
+        "False",
+    )
+    q4 = TrueOrFalseQA(
+        "Are computers electronic?", "True", "General CS", "True", "False"
+    )
 
     # Test if q1 and q4 have the same hash, but different hashes from q2 and q3
     assert hash(q1) == hash(q4)
@@ -210,12 +265,42 @@ if __name__ == "__main__":
     print("***** break *****")
 
     # Test hashing for MultipleChoiceQA
-    q1 = MultipleChoiceQA("What does the R in RAM stand for?", "Random", "General CS", "Random", "Rye", "Robot", "Ruby")
-    q2 = MultipleChoiceQA("What does the A in RAM stand for?", "Access", "General CS", "Aardvark", "Access", "Area",
-                          "Aliens")
-    q3 = MultipleChoiceQA("What does the M in RAM stand for?", "Memory", "General CS", "Martian", "Money", "Machine",
-                          "Memory")
-    q4 = MultipleChoiceQA("What does the R in RAM stand for?", "Random", "General CS", "Random", "Rye", "Robot", "Ruby")
+    q1 = MultipleChoiceQA(
+        "What does the R in RAM stand for?",
+        "Random",
+        "General CS",
+        "Random",
+        "Rye",
+        "Robot",
+        "Ruby",
+    )
+    q2 = MultipleChoiceQA(
+        "What does the A in RAM stand for?",
+        "Access",
+        "General CS",
+        "Aardvark",
+        "Access",
+        "Area",
+        "Aliens",
+    )
+    q3 = MultipleChoiceQA(
+        "What does the M in RAM stand for?",
+        "Memory",
+        "General CS",
+        "Martian",
+        "Money",
+        "Machine",
+        "Memory",
+    )
+    q4 = MultipleChoiceQA(
+        "What does the R in RAM stand for?",
+        "Random",
+        "General CS",
+        "Random",
+        "Rye",
+        "Robot",
+        "Ruby",
+    )
 
     # Test if q1 and q4 have the same hash, but different hashes from q2 and q3
     assert hash(q1) == hash(q4)
@@ -245,10 +330,18 @@ if __name__ == "__main__":
     print("***** break *****")
 
     # Test hashing for ShortAnswerQA
-    q1 = ShortAnswerQA("What does the M in MVC stand for?", "Model", "Design Patterns")
-    q2 = ShortAnswerQA("What does the V in MVC stand for?", "View", "Design Patterns")
-    q3 = ShortAnswerQA("What does the C in MVC stand for?", "Controller", "Design Patterns")
-    q4 = ShortAnswerQA("What does the M in MVC stand for?", "Model", "Design Patterns")
+    q1 = ShortAnswerQA(
+        "What does the M in MVC stand for?", "Model", "Design Patterns"
+    )
+    q2 = ShortAnswerQA(
+        "What does the V in MVC stand for?", "View", "Design Patterns"
+    )
+    q3 = ShortAnswerQA(
+        "What does the C in MVC stand for?", "Controller", "Design Patterns"
+    )
+    q4 = ShortAnswerQA(
+        "What does the M in MVC stand for?", "Model", "Design Patterns"
+    )
 
     # Test if q1 and q4 have the same hash, but different hashes from q2 and q3
     assert hash(q1) == hash(q4)
