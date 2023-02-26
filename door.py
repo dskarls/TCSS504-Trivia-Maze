adca74cc594fb870ae44f4601b9fe15758405b9a
dskarls <dskpersistent@gmail.com>
2023-02-25 18:53:02 -0800

    API: Have doors initialized with just Q&A

--- door.py
+++ door.py
@@ -3,20 +3,20 @@
 
 class Door:
     """
-    This class represents a single door for a single room. A Door will store
-    a QuestionAndAnswer object.
+    This class represents a single door for a single room. A Door may store a
+    QuestionAndAnswer object.
 
     Attributes
     ----------
     question_and_answer : QuestionAndAnswer
-        Reference to the QuestionAndAnswer class
+        An object representing a question and answer.
     locked : bool
         The locked state of a Door. True for locked, False for unlocked.
     perm_locked : bool
         If QuestionAndAnswer receives wrong answer door becomes permanently locked.
     """
 
-    def __init__(self, locked=True):
-        self.locked = locked
+    def __init__(self, question_and_answer=None):
+        self.question_and_answer = question_and_answer
+        self.locked = True if question_and_answer else False
         self.perm_locked = False
-        self.question_and_answer = None
