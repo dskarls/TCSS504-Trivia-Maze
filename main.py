7859648fd9addb7d20415ee24d66d10e9a613d0d
dskarls <dskpersistent@gmail.com>
2023-02-25 18:44:26 -0800

    MAINT: Switch to using real model in driver
    The model can now be instantiated without failing, so we can use it

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from mock import TextTriviaMazeModel  # FIXME: Set to real model module
+from trivia_maze import TriviaMaze
 from text_trivia_maze_controller import TextTriviaMazeController
 
 if __name__ == "__main__":
-    maze_model = TextTriviaMazeModel()
+    NUM_ROWS = 5
+    NUM_COLS = 7
+    maze_model = TriviaMaze(NUM_ROWS, NUM_COLS)
 
     # NOTE: The controller will create a view object internally
     maze_controller = TextTriviaMazeController(maze_model)
