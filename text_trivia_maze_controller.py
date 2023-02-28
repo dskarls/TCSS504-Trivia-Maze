f3739e2791c8b4c14d818c1b1caf50e93be84d60
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2023-02-28 14:25:54 -0800

    Missing underscores

--- text_trivia_maze_controller.py
+++ text_trivia_maze_controller.py
@@ -135,10 +135,10 @@ class TextTriviaMazeController(TriviaMazeController):
         # Model update calls
         game_status = self._maze_model.game_status()
         if game_status == "lose":
-            self._maze_view.show_game_lost_menu()
+            self.__maze_view.show_game_lost_menu()
             self.set_active_context("game_lost_menu")
         elif game_status == "win":
-            self._maze_view.show_game_won_menu()
+            self.__maze_view.show_game_won_menu()
             self.set_active_context("game_won_menu")
         self.__process_question_and_answer_buffer()
 
